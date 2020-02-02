63cb0c85385fdbecc2ec86b079a0247a17b4b494
Hugo <hugo.filipereis@hotmail.com>
2020-02-02 18:42:55 +0000

    Interaction with objects
    Key E to pick up

---
+++ CitrusX/Assets/Scripts/Interact.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interact : MonoBehaviour
+{
+    public int RayRange;
+    RaycastHit Hit;
+
+    private void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Physics.Raycast(transform.position,transform.forward,out Hit,RayRange))
+        {
+            if(Hit.transform.tag == "Object")
+            {
+                //TextBox.text = "Press E to pick up";
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    Vector3 Pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    Vector2 TouchPos = new Vector2(Pos.x, Pos.y);
+
+                    if (TouchPos.x == Pos.x && TouchPos.y == Pos.y)
+                    {
+                        Destroy(Hit.transform.gameObject);
+                    }
+                }
+            }
+        }
+    }
+}
