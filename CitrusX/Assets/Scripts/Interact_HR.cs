3e918b48282f6701035c76682d53e60587a086d3
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2020-02-04 15:58:45 +0000

    Optimising and Review of Interaction_HR

--- CitrusX/Assets/Scripts/Interact_HR.cs
+++ CitrusX/Assets/Scripts/Interact_HR.cs
@@ -1,36 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/* Hugo
+ * 
+ * What script does...
+ * 
+ * Dominique (Changes) 04/02/2020
+ * Specified Hit and UI as private for a consistent coding standard
+ * Got rid of unused imported namespaces (System.Collections and System.Collections.Generic)
+ * Renamed variable (and game object in hierarchy) to NotificationText
+ * Set rayRange to 5 in script
+ * Optimising after 'if(Input.GetKeyDown(KeyCode.E))' -> Unnecessary if statement that checked if the Vector3 mouse position equaled a Vector2 cast of itself
+ */
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Interact_HR : MonoBehaviour
 {
-    public int RayRange;
-    RaycastHit Hit;
-    Text UI;
+    public int rayRange = 5;
+
+    private RaycastHit hit;
+    private Text notificationText;
+
     private void Awake()
     {
-        UI = GameObject.Find("UIText").GetComponent<Text>();
+        notificationText = GameObject.Find("NotificationText").GetComponent<Text>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Physics.Raycast(transform.position,transform.forward,out Hit,RayRange))
+        if(Physics.Raycast(transform.position, transform.forward, out hit, rayRange))
         {
-            if(Hit.transform.tag == "Object")
+            if(hit.transform.tag == "Object")
             {
-                UI.text = "Press E to pick up";
+                notificationText.text = "Press E to pick up";
                 if (Input.GetKeyDown(KeyCode.E))
                 {
-                    Vector3 Pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                    Vector2 TouchPos = new Vector2(Pos.x, Pos.y);
-
-                    if (TouchPos.x == Pos.x && TouchPos.y == Pos.y)
-                    {
-                        Destroy(Hit.transform.gameObject);
-                        UI.text = "";
-                    }
+                    Destroy(hit.transform.gameObject);
+                    notificationText.text = "";
                 }
             }
         }
