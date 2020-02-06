0211d4c70e68097837c82299489c554950818fa7
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2020-02-06 15:45:12 +0000

    Started Keypad Implementation

---
+++ CitrusX/Assets/Scripts/KeypadUI_DR.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class KeypadUI_DR : MonoBehaviour
+{
+    private string input;
+
+    public void SetInput(string to) { input = to; }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (!doorUnlocked)
+        {
+            notificationText.text = "Press E to use the keypad";
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                OpenKeypad();
+            }
+        }
+    }
+}
