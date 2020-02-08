9a50d7c6a8e2469f1c2744df3a40effc9dc637fc
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2020-02-08 14:58:32 +0000

    Started setting up chess puzzle system
    Can read book and place piece on board
    Need to add rotating functionality and check that all pieces are rotated correctly

---
+++ CitrusX/Assets/Scripts/ChessBoard_DR.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessBoard_DR : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Journal_DR.instance.ChangeTasks(new string[]{"Pawn",});
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
