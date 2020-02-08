66cd63a7460bf520a2703c2dc3f9b79d79df1525
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2020-02-08 14:59:53 +0000

    Some changes to scripts weren't saved

--- CitrusX/Assets/Scripts/ChessBoard_DR.cs
+++ CitrusX/Assets/Scripts/ChessBoard_DR.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ChessBoard_DR : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Transform[] chessPieces;
+
     void Start()
     {
         Journal_DR.instance.ChangeTasks(new string[]{"Pawn",});
+
+        //Get the transforms of all the chess pieces (don't include the board itself)
+        Transform[] transforms = GetComponentsInChildren<Transform>();
+        chessPieces = new Transform[transforms.Length - 1];
+        int count = 0;
+        for(int i = 0; i < transforms.Length; i++)
+        {
+            if(transforms[i].tag == "ChessPiece")
+            {
+                chessPieces[count] = transforms[i];
+                ++count;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    //Rotate pieces by 90 degrees
+    public void RotatePiece(string name)
     {
-        
+        for (int i = 0; i < chessPieces.Length; i++)
+        {
+            if(chessPieces[i].name == name)
+            {
+                chessPieces[i].Rotate(90, 0, 0);
+            }
+        }
     }
 }
