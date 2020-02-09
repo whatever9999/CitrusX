a3de9deee855e7fadcc360d7794ef833b3df1ee5
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2020-02-09 14:15:43 +0000

    Continue chess puzzle

---
+++ CitrusX/Assets/Scripts/ChessPuzzleJournal_DR.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessPuzzleJournal_DR : MonoBehaviour
+{
+    private void Start()
+    {
+        Journal_DR.instance.ChangeTasks(new string[] { "Pawn" });
+    }
+}
