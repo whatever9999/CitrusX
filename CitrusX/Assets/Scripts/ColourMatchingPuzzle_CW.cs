9d7b786d83f06e8c37f156e948f6b3e6894ebfc5
AGordon93 <60220429+AGordon93@users.noreply.github.com>
2020-02-11 23:48:55 +0000

    Merge "Master" into "Adam"

--- CitrusX/Assets/Scripts/ColourMatchingPuzzle_CW.cs
+++ CitrusX/Assets/Scripts/ColourMatchingPuzzle_CW.cs
@@ -2,24 +2,28 @@
  * Colour matching script
  * This scripts main function 'Start colour Matching puzzle' can be called during game play to add it to the journal and start the process
  * when this puzzle begins
- * Attach this script anywhere in the scene for it to work
+
  * 
  * Dominique (Changes) 11/02/2020
  * Removed unused package imports
+
  */
 using UnityEngine;
 
 public class ColourMatchingPuzzle_CW : MonoBehaviour
 {
     private Journal_DR journal;
+    private bool isActive = false;
+    internal void SetActive(bool value) { isActive = false; }
     public void Awake()
     {
-        journal = GameObject.Find("FPSController").GetComponent<Journal_DR>();
-        StartColourMatchingPuzzle(); //only used for testing
+        journal = Journal_DR.instance;
     }
-    internal void StartColourMatchingPuzzle()
+    private void Update()
     {
-        journal.AddJournalLog("This door looks like it needs a key...maybe I should try the garage");
-        journal.ChangeTasks(new string[] { "key part 1", "key part 2" });
+        if(journal.AreTasksComplete())
+        {
+            GameTesting_CW.instance.arePuzzlesDone[2] = true;
+        }
     }
 }
