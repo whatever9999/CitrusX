2fec0f55fadb73c5fb1f7cd185a17c418118f3b3
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2020-02-25 18:10:21 +0000

    Turned dictionaries into struct for subtitles and readded values in inspector

--- CitrusX/Assets/Scripts/Subtiles_HR.cs
+++ CitrusX/Assets/Scripts/Subtiles_HR.cs
@@ -3,17 +3,16 @@
  * 
  * Other scripts can call the function with the id of the clip and subtitles they wanna show on screen
  * 
+ * Dominique (Changes) 25/02/2020
+ * Changed subtitles from two dictionaries into a struct so this is easier to use in the inspector and less open for error
  */
 
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Subtiles_HR : MonoBehaviour
 {
-
-    //Change this chase
     public enum ID
     {
         P1_LINE1,
@@ -97,50 +96,56 @@ public class Subtiles_HR : MonoBehaviour
         GASP
     }
 
-    //With the same key (the enumerator) you can acess both subtitles and clips
-    public List<ID> audioID = new List<ID>();
-    public List<AudioClip> voiceClips = new List<AudioClip>();
-    public List<string> subtitles = new List<string>();
+    //This array will show in the inspector for subtitle data to be passed in to
+    public Subtitle[] subtitles;
 
-    private Dictionary<ID, string> subtitlesDictionary = new Dictionary<ID, string>();
-    private Dictionary<ID, AudioClip> clipDictionary = new Dictionary<ID, AudioClip>();
     private Text subtitleText;
     private AudioSource voiceSource;
-    
+
     void Awake()
     {
         voiceSource = GetComponent<AudioSource>();
         subtitleText = GameObject.Find("Subtitles").GetComponent<Text>();
 
-        //Load both subtitles and clips
-        for (int i = 0; i < voiceClips.Count; i++)
-        {
-            clipDictionary.Add(audioID[i], voiceClips[i]);
-        }
-
-        for (int i = 0; i < subtitles.Count; i++)
-        {
-            subtitlesDictionary.Add(audioID[i], subtitles[i]);
-        }
-
         subtitleText.text = "";
     }
 
-    //THE BIG FUNCTION TO CALL CHASE
-    public void PlayAudio(ID id) 
+    void Start()
     {
-        voiceSource.clip = clipDictionary[id];
-        subtitleText.text = subtitlesDictionary[id];
+        //TEST
+        //PlayAudio(ID.P1_LINE1);
+    }
+
+    //THE BIG FUNCTION TO CALL CHASE
+    public void PlayAudio(ID id)
+    {
+        for (int i = 0; i < subtitles.Length; i++)
+        {
+            if (subtitles[i].id == id)
+            {
+                voiceSource.clip = subtitles[i].clip;
+                subtitleText.text = subtitles[i].text;
+                break;
+            }
+        }
         voiceSource.Play();
         SubtitleReset(voiceSource.clip.length);
-       
     }
 
 
     //Wait for the clip to be over to remove text
-    IEnumerator SubtitleReset(float timeToWait) 
+    IEnumerator SubtitleReset(float timeToWait)
     {
         yield return new WaitForSeconds(timeToWait);
         subtitleText.text = "";
     }
+}
+
+//Each subtitle has an ID, clip and the text that links to the clip
+[System.Serializable]
+public struct Subtitle
+{
+    public Subtiles_HR.ID id;
+    public AudioClip clip;
+    public string text;
 }
