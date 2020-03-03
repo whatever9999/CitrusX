599fe31a3bf00e0de30eded7fb24ca2b77be2f93
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2020-03-03 21:34:19 +0000

    Fusebox can be turned and there is a button to check if it is complete

--- CitrusX/Assets/Scripts/Puzzles/Fusebox_CW/Fusebox_CW.cs
+++ CitrusX/Assets/Scripts/Puzzles/Fusebox_CW/Fusebox_CW.cs
@@ -9,6 +9,9 @@
  *
  * Chase(Changes) 22/2/2020
  * Added voiceover comments and bools
+ * 
+ * Dominique (Changes) 03/03/2020
+ * Moved and changed directions, simplified script a bit, seperated the commented wires code a bit
 */
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
@@ -16,24 +19,37 @@ using UnityEngine.UI;
 
 public class Fusebox_CW : MonoBehaviour
 {
-    #region VARIABLES
+    public enum Directions
+    {
+        HORIZONTAL,
+        VERTICAL,
+        RIGHT_DOWN_BEND,
+        LEFT_DOWN_BEND,
+        RIGHT_UP_BEND,
+        LEFT_UP_BEND
+    }
+
+    #region PuzzleVariables
+    public KeyCode closeFuseboxKey = KeyCode.Z;
+    public KeyCode resetPipesKey = KeyCode.X;
+
     private FirstPersonController fpsController;
-    private bool isFuseboxSolved = false;
-    private bool isActive = false;
-    private KeyCode closeFuseboxKey = KeyCode.Z;
-    internal KeyCode resetPipesKey = KeyCode.X;
-    internal int pipeCompletedCount;
-    //internal int wireCompletedCount;
     private Text fuseboxText;
-    public Pipes_CW[] pipes;
-    public Pipes_CW[] wires;
-    internal Color drawColour;
+    private Pipes_CW[] pipes;
+    private GameObject fusebox;
+    //public Pipes_CW[] wires;
+    //private int wireCompletedCount;
+    //private Color drawColour;
+    #endregion
+
+    #region GameStateVariables
     private Journal_DR journal;
     private bool[] voiceovers = { false, false };
     private Subtiles_HR subtitles;
+    internal bool isFuseboxSolved = false;
+    private bool isActive = false;
     #endregion
     internal bool GetState() { return isFuseboxSolved; }
-
     internal void SetGameActive(bool value) { isActive = value; }
 
     void Awake()
@@ -42,17 +58,17 @@ public class Fusebox_CW : MonoBehaviour
         journal = Journal_DR.instance;
         fuseboxText = GameObject.Find("FuseboxMessageText").GetComponent<Text>();
         subtitles = GameObject.Find("FirstPersonCharacter").GetComponent<Subtiles_HR>();
-    }
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        pipes = GameObject.FindObjectsOfType<Pipes_CW>();
+        fusebox = GameObject.Find("Fusebox");
+
         gameObject.SetActive(false);
     }
     private void Update()
     {
-        GetAllPipesInScene();
-        CheckForClose();
+        if (Input.GetKeyDown(closeFuseboxKey))
+        {
+            CloseFusebox();
+        }
     }
 
 
@@ -68,13 +84,23 @@ public class Fusebox_CW : MonoBehaviour
         //Stop the player from moving while using the fusebox
         fpsController.enabled = false;
     }
-    public void GetAllPipesInScene()
+    public void CheckPipes()
     {
         //set the pipes in the inspector
         //set the wire ends into the wires array in the inspector also
-        if (pipeCompletedCount == pipes.Length)
+        bool complete = true;
+        for(int i = 0; i < pipes.Length; i++)
+        {
+            if(!pipes[i].GetIsInPosition())
+            {
+                complete = false;
+            }
+        }
+
+        if (complete)
         {
             fuseboxText.text = "COMPLETED";
+            isFuseboxSolved = true;
             journal.TickOffTask("Fix fusebox");
             if (!voiceovers[1])
             {
@@ -82,20 +108,13 @@ public class Fusebox_CW : MonoBehaviour
                 voiceovers[1] = true;
                 GameTesting_CW.instance.arePuzzlesDone[1] = true;
             }
-        }
-    }
-    private void CheckForClose()
-    {
-        if (Input.GetKeyDown(closeFuseboxKey))
-        {
-            CloseFusebox();
+
+            //Player can't use the fusebox anymore
+            fusebox.tag = "Untagged";
         }
     }
     public void CloseFusebox()
     {
-        //Make sure raycasts know the fusebox is a fusebox
-        gameObject.tag = "Fusebox";
-
         //Make the cursor invisible again
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
--- CitrusX/Assets/Scripts/Puzzles/Fusebox_CW/Pipes_CW.cs
+++ CitrusX/Assets/Scripts/Puzzles/Fusebox_CW/Pipes_CW.cs
@@ -6,228 +6,89 @@
 * Dominique (Changes) 11/02/2020
 * Removed unused package imports
 * Moved rotate in switch case to before (stops repetition)
+* 
 * Chase (Changes) 19/2/2020
 * Commented out all wire code and stuck to just pipes for the time being
+* 
+* Dominique (Changes) 03/03/2020
+* Seperated pipes and wires and simplified script so most is handled by Fusebox
 */
 
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Pipes_CW : MonoBehaviour
 {
-    #region CONNECTING_WIRES
-    public Button northWire;
-    public Button southWire;
-    public Button eastWire;
-    public Button westWire;
-    public Pipes_CW northWireScript;
-    public Pipes_CW southWireScript;
-    public Pipes_CW eastWireScript;
-    private Pipes_CW westWireScript;
-    #endregion
-    #region VARIABLES
-    public enum DIRECTIONS
-    {
-        HORIZONTAL,
-        VERTICAL,
-        RIGHT_DOWN_BEND,
-        LEFT_DOWN_BEND,
-        RIGHT_UP_BEND,
-        LEFT_UP_BEND
-    }
-    public enum COLOURS
-    {
-        RED,
-        BLUE,
-        GREEN
-    }
-    public DIRECTIONS currentDirection;
-    public DIRECTIONS desiredDirection;
-    public COLOURS wireEndColour;
-    public bool isWireEnd;
-    public bool isPipe;
-    public Button matchingEnd;
-    private Button thisPipe;
-    public bool[] wiresConnectedTo =  { false, false, false, false};
-    private bool isInPosition = false;
-    private bool isWireConnected = false;
+    public Fusebox_CW.Directions startPosition;
+    private Fusebox_CW.Directions currentPosition;
+    public Fusebox_CW.Directions desiredPosition;
+
     private const int degreesToMove = 90;
-    private Color wireColour;
-    Color defaultBoxColour;
-    private Fusebox_CW theFusebox;
-    #endregion
-    public bool GetCompletionState() { return isInPosition; }
+    private Fusebox_CW fusebox;
+
+    public bool GetIsInPosition() {
+        if (currentPosition == desiredPosition)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
     public void Awake()
     {
-        theFusebox = GameObject.Find("FuseboxUI").GetComponent<Fusebox_CW>();
-        defaultBoxColour = GetComponent<Button>().image.color;
-        wireColour = GetComponent<Button>().image.color;
-        thisPipe = GetComponent<Button>();
+        fusebox = GameObject.Find("FuseboxUI").GetComponent<Fusebox_CW>();
+        currentPosition = startPosition;
     }
-    public void Start()
-    {
-        if(wiresConnectedTo[0])
-        {
-            northWireScript = northWire.GetComponent<Pipes_CW>(); 
-        }
-        if(wiresConnectedTo[1])
-        {
-            eastWireScript = eastWire.GetComponent<Pipes_CW>();
-        }
-        if(wiresConnectedTo[2])
-        {
-            southWireScript = southWire.GetComponent<Pipes_CW>();
-        }
-        if(wiresConnectedTo[3])
-        {
-            westWireScript = westWire.GetComponent<Pipes_CW>();
-        }
-       
-
-    }
+    
     public void Update()
     {
-        //if X, reset puzzle to default colours and state
-        if(Input.GetKeyDown(theFusebox.resetPipesKey))
+        //if X, reset puzzle to default colours and state (make sure they can't do this if they've already solved it)
+        if(!fusebox.isFuseboxSolved && Input.GetKeyDown(fusebox.resetPipesKey))
         {
-            wireColour = defaultBoxColour;
-            if(!isWireEnd)
+            while(currentPosition != startPosition)
             {
-                isWireConnected = false;
-            }  
-        }
-    }
-    public void Rotate()
-    {
-        //if in the desired position set to true so it will not move
-        if(desiredDirection == currentDirection)
-        {
-            isInPosition = true;
-            thisPipe.enabled = false;
-            theFusebox.pipeCompletedCount++;
-        }
-
-        //if not in correct position, rotate it
-        if (desiredDirection != currentDirection)
-        {
-            gameObject.transform.Rotate(0, 0, degreesToMove);
-
-            switch (currentDirection)
-            {
-                case DIRECTIONS.HORIZONTAL:
-                    { 
-                        currentDirection = DIRECTIONS.VERTICAL; 
-                    }
-                    break;
-                case DIRECTIONS.VERTICAL:
-                    {
-                        currentDirection = DIRECTIONS.HORIZONTAL;
-                    }
-                    break;
-                case DIRECTIONS.RIGHT_DOWN_BEND:
-                    {
-                        currentDirection = DIRECTIONS.LEFT_DOWN_BEND;
-                    }
-                    break;
-                case DIRECTIONS.LEFT_DOWN_BEND:
-                    {
-                        currentDirection = DIRECTIONS.LEFT_UP_BEND;
-                    }
-                    break;
-                case DIRECTIONS.RIGHT_UP_BEND:
-                    {
-                        currentDirection = DIRECTIONS.RIGHT_DOWN_BEND;
-                    }
-                    break;
-                case DIRECTIONS.LEFT_UP_BEND:
-                    {
-                        currentDirection = DIRECTIONS.RIGHT_UP_BEND;
-                    }
-                    break;
-                default:
-                    break;
+                Rotate();
             }
         }
-       
     }
-    //public void ConnectWires()
-    //{
-    //    //check if it is a wireend (this is ticked in the inspector)
-    //    if(isWireEnd)
-    //    {
-    //        //signify that it is already connected
-    //        isWireConnected = true;
-    //        //check for what colour it represents
-    //        switch (wireEndColour)
-    //        {
-    //            case COLOURS.RED:
-    //                {
-    //                    //then set the drawcolour to the correct colour
-    //                    theFusebox.drawColour = Color.red;
-    //                }
-    //                break;
-    //            case COLOURS.BLUE:
-    //                {
-    //                    theFusebox.drawColour = Color.blue;
-    //                }
-    //                break;
-    //            case COLOURS.GREEN:
-    //                {
-    //                    theFusebox.drawColour = Color.green;
-    //                }
-    //                break;
-    //            default:
-    //                break;
-    //        }
-    //        if ((wiresConnectedTo[0] && northWireScript.wireEndColour == wireEndColour) || (wiresConnectedTo[1] && eastWireScript.wireEndColour == wireEndColour) ||(wiresConnectedTo[2] && southWireScript.wireEndColour == wireEndColour) || (wiresConnectedTo[3] && westWireScript.wireEndColour == wireEndColour))
-    //        {
-    //            //if the previous tile is the same colour as the wire end, both the original and last pipe end will change colour to show
-    //            //completion
-    //            wireColour = Color.yellow;
-    //            matchingEnd.image.color = Color.yellow;
-    //            northWireScript.isWireConnected = true;
-    //            southWireScript.isWireConnected = true;
-    //            eastWireScript.isWireConnected = true;
-    //            westWireScript.isWireConnected = true;
-    //            theFusebox.wireCompletedCount += 1;
-    //        }    
-    //    }
-    //    if(!isWireEnd)
-    //    {
-    //        //if the tile hasn't been manipulated
-    //        if(wireColour == defaultBoxColour)
-    //        {
-    //            //check if the previous pipe has been used
-    //            if(( wiresConnectedTo[1] && eastWireScript.isWireConnected) || ( wiresConnectedTo[0] && northWireScript.isWireConnected) ||
-    //                (wiresConnectedTo[2] && southWireScript.isWireConnected) || (wiresConnectedTo[3] && eastWireScript.isWireConnected))
-    //            {
-    //                //set the wireEndColour for the comparison for wireEnds
-    //                if (theFusebox.drawColour == Color.red)
-    //                {
-    //                    wireEndColour = COLOURS.RED;
-                        
-    //                }
-    //                else if (theFusebox.drawColour == Color.green)
-    //                {
-    //                    wireEndColour = COLOURS.GREEN;
-    //                }
-    //                else if (theFusebox.drawColour == Color.blue)
-    //                {
-    //                    wireEndColour = COLOURS.BLUE;
-    //                }
-    //                //check for colour
-    //                if ((wiresConnectedTo[0] && northWireScript.wireEndColour == wireEndColour) || (wiresConnectedTo[1] && eastWireScript.wireEndColour == wireEndColour) 
-    //                    || (wiresConnectedTo[2] && southWireScript.wireEndColour == wireEndColour) || (wiresConnectedTo[3] && westWireScript.wireEndColour == wireEndColour))
-    //                {
-    //                    //draw the correct colour tile and signify as connected
-    //                    wireColour = theFusebox.drawColour;
-    //                    thisPipe.image.color = theFusebox.drawColour;
-    //                    isWireConnected = true;
-    //                }
-                   
-    //            }
-    //        }
-            
-    //    }
-    //}
+
+    public void Rotate()
+    {
+        gameObject.transform.Rotate(0, 0, degreesToMove);
+
+        switch (currentPosition)
+        {
+            case Fusebox_CW.Directions.HORIZONTAL:
+                {
+                    currentPosition = Fusebox_CW.Directions.VERTICAL;
+                }
+                break;
+            case Fusebox_CW.Directions.VERTICAL:
+                {
+                    currentPosition = Fusebox_CW.Directions.HORIZONTAL;
+                }
+                break;
+            case Fusebox_CW.Directions.RIGHT_DOWN_BEND:
+                {
+                    currentPosition = Fusebox_CW.Directions.RIGHT_UP_BEND;
+                }
+                break;
+            case Fusebox_CW.Directions.LEFT_DOWN_BEND:
+                {
+                    currentPosition = Fusebox_CW.Directions.RIGHT_DOWN_BEND;
+                }
+                break;
+            case Fusebox_CW.Directions.RIGHT_UP_BEND:
+                {
+                    currentPosition = Fusebox_CW.Directions.LEFT_UP_BEND;
+                }
+                break;
+            case Fusebox_CW.Directions.LEFT_UP_BEND:
+                {
+                    currentPosition = Fusebox_CW.Directions.LEFT_DOWN_BEND;
+                }
+                break;
+        }
+    }
 }
