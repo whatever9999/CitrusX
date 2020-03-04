ffc3b4c8d082e1eb3a56102b6edeed4aea81ed9c
alrobbo <33253689+alrobbo@users.noreply.github.com>
2020-03-04 18:49:26 +0000

    added animation and assets to the menu
    I added a mansion asset to the manu and animation on the camera so it zooms into the mansion.

--- CitrusX/Assets/Scripts/UI/UIManager_AR.cs
+++ CitrusX/Assets/Scripts/UI/UIManager_AR.cs
@@ -7,17 +7,41 @@
  * 
  * Alex 
  * Added the SFX manager toggle that Dominique told me to add
+ * 
+ * Alex Added PlayAnimation funtion to enable the animation 
+ * for the camera also added a timer to the load scene function so that
+ * the aniamtion has time to play before the scene is loaded
  */
 
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class UIManager_AR : MonoBehaviour
 {
+    public float Timer = 5;
+    public bool TimerStart = false;
+
+    private void Update()
+    {
+        if (TimerStart == true)
+        {
+            Timer -= Time.deltaTime;
+        }
+
+
+    }
+
     public void LoadByIndex(int sceneIndex)
     {
-        SceneManager.LoadScene(sceneIndex);
+        
+        
+        if (Timer <= 0)
+        {
+           SceneManager.LoadScene(sceneIndex);
+        }
+        
     }
 
     public void QuitButton()
@@ -28,5 +52,12 @@ public class UIManager_AR : MonoBehaviour
     public void SFXToggle()
     {
         SFXManager_DR.instance.ToggleSFX();
+    }
+
+    public void PLayAnimation()
+    {
+        GetComponent<Animator>().SetBool("Play", true);
+        TimerStart = true;
+
     }
 }
