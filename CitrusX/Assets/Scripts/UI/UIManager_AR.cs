e4d455e32536607c925b966a59659bda685cb1e2
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2020-03-04 10:07:49 +0000

    Added menu backgrounds

--- CitrusX/Assets/Scripts/UI/UIManager_AR.cs
+++ CitrusX/Assets/Scripts/UI/UIManager_AR.cs
@@ -4,6 +4,9 @@
  * 
  * Dominique
  * Added a quit function and changed the script to a UIManager
+ * 
+ * Dominique 03/03/2020
+ * Added a function that gets the SFXManager in the scene and turns the sound on/off
  */
 
 
@@ -12,9 +15,15 @@ using UnityEngine.SceneManagement;
 
 public class UIManager_AR : MonoBehaviour
 {
+
     public void LoadByIndex(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void ToggleSFX()
+    {
+        GameObject.Find("SFXManager").GetComponent<SFXManager_DR>().ToggleSFX();
     }
 
     public void QuitButton()
