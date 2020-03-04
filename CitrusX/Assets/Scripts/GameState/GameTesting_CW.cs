3b76327a19aa957645d2477b470d4a7db1e1ab4a
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2020-03-04 15:04:36 +0000

    Revert "Revert "Chase to Master""
    This reverts commit 62824129e8f4259ea4d7c30f85b00fbd7c1463c6.

--- CitrusX/Assets/Scripts/GameState/GameTesting_CW.cs
+++ CitrusX/Assets/Scripts/GameState/GameTesting_CW.cs
@@ -7,6 +7,8 @@
  * 
  * Chase (Changes) 2/3/2020
  * Added disturbances, changed initiate to a reference instead of calling the instance every time
+ * Chase (Changes) 4/3/2020
+ * Added GameObjects for puzzles, tidied script up aswell
  */
 
 using System.Collections;
@@ -15,30 +17,49 @@ using UnityEngine;
 
 public class GameTesting_CW : MonoBehaviour
 {
-    public static GameTesting_CW instance;
+    #region TRIGGERS
     private TriggerScript_CW ritualTrigger;
     private TriggerScript_CW chessTrigger;
     private TriggerScript_CW throwingTrigger;
+    private TriggerScript_CW correctOrderTrigger;
+    #endregion
+    #region BOOLS
     private bool[] setUpPuzzle = { false, false, false, false, false, false, false, false, false, false };
     internal bool[] arePuzzlesDone = { false, false, false, false, false, false, false, false, false, false, false };
     private bool[] cutscenes = { false, false, false };
     private bool[] cutscenesDone = { false, false, false };
+    #endregion
+    #region OTHER_VARIABLES
     private DisturbanceHandler_DR disturbance;
     private InitiatePuzzles_CW initiate;
+    GameObject throwingBox;
+    GameObject hiddenMechDoc;
+    #endregion
+
+    public static GameTesting_CW instance;
 
     private void Awake()
     {
         instance = this;
         disturbance = DisturbanceHandler_DR.instance;
         initiate = InitiatePuzzles_CW.instance;
+        throwingBox = GameObject.Find("ThrowingBox");
+        hiddenMechDoc = GameObject.Find("HiddenMechNote");
+        #region INTIALISE_TRIGGERS
         ritualTrigger = GameObject.Find("RitualTrigger").GetComponent<TriggerScript_CW>();
         chessTrigger = GameObject.Find("ChessboardTrigger").GetComponent<TriggerScript_CW>();
         throwingTrigger = GameObject.Find("ThrowingTrigger").GetComponent<TriggerScript_CW>();
+        correctOrderTrigger = GameObject.Find("CorrectOrderTrigger").GetComponent<TriggerScript_CW>();
+        #endregion
     }
-    // Update is called once per frame
+    private void Start()
+    {
+        throwingBox.SetActive(false);
+        hiddenMechDoc.SetActive(false);
+    }
+
     void Update()
     {
-        //play start cutscene
         if (!cutscenes[0])
         {
             //play start cutscene
@@ -74,7 +95,7 @@ public class GameTesting_CW : MonoBehaviour
         {
             setUpPuzzle[5] = true;
             chessTrigger.allowedToBeUsed = true;
-            disturbance.TriggerDisturbance(DisturbanceHandler_DR.DisturbanceName.PAWNFALL); //might move to monitor interaction
+           // disturbance.TriggerDisturbance(DisturbanceHandler_DR.DisturbanceName.PAWNFALL); //might move to monitor interaction
             initiate.InitiateChessBoardPuzzle();
         }
         else if (arePuzzlesDone[5] && !setUpPuzzle[6])
@@ -87,14 +108,17 @@ public class GameTesting_CW : MonoBehaviour
         }
         else if (arePuzzlesDone[6] && !setUpPuzzle[7])
         {
+            throwingBox.SetActive(true);
             setUpPuzzle[7] = true;
-            disturbance.TriggerDisturbance(DisturbanceHandler_DR.DisturbanceName.LAMPWOBBLE);
+            //disturbance.TriggerDisturbance(DisturbanceHandler_DR.DisturbanceName.LAMPWOBBLE);
             initiate.InitiateHiddenMechanismPuzzle();
         }
         else if (arePuzzlesDone[7] && !setUpPuzzle[8])
         {
+            hiddenMechDoc.SetActive(true);
             setUpPuzzle[8] = true;
-            disturbance.TriggerDisturbance(DisturbanceHandler_DR.DisturbanceName.DOORCREAK);
+            correctOrderTrigger.allowedToBeUsed = true;
+         //   disturbance.TriggerDisturbance(DisturbanceHandler_DR.DisturbanceName.DOORCREAK);
             initiate.InitiateCorrectOrderPuzzle();
         }
         else if (arePuzzlesDone[8] && !setUpPuzzle[9])
