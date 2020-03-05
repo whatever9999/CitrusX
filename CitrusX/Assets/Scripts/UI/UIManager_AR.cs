14445d2f20e13e5a952a43bbe70a1b8cd5170b72
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2020-03-05 13:24:45 +0000

    Ensured all menu stuff is plugged in

--- CitrusX/Assets/Scripts/UI/UIManager_AR.cs
+++ CitrusX/Assets/Scripts/UI/UIManager_AR.cs
@@ -5,6 +5,13 @@
  * Dominique
  * Added a quit function and changed the script to a UIManager
  * 
+ * Alex 
+ * Added the SFX manager toggle that Dominique told me to add
+ * 
+ * Alex Added PlayAnimation funtion to enable the animation 
+ * for the camera also added a timer to the load scene function so that
+ * the aniamtion has time to play before the scene is loaded
+ * 
  * Dominique 03/03/2020
  * Added a function that gets the SFXManager in the scene and turns the sound on/off
  */
@@ -13,12 +20,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class UIManager_AR : MonoBehaviour
 {
 
+    public float Timer = 5;
+    public bool TimerStart = false;
+
+    private void Update()
+    {
+        if (TimerStart == true)
+        {
+            Timer -= Time.deltaTime;
+        }
+    }
+
     public void LoadByIndex(int sceneIndex)
     {
-        SceneManager.LoadScene(sceneIndex);
+        if (Timer <= 0)
+        {
+           SceneManager.LoadScene(sceneIndex);
+        }
+        
     }
 
     public void ToggleSFX()
@@ -29,5 +52,16 @@ public class UIManager_AR : MonoBehaviour
     public void QuitButton()
     {
         Application.Quit();
+    }
+
+    public void SFXToggle()
+    {
+        SFXManager_DR.instance.ToggleSFX();
+    }
+
+    public void PLayAnimation()
+    {
+        Camera.main.GetComponent<Animator>().SetBool("Play", true);
+        TimerStart = true;
     }
 }
