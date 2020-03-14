3e0766761b6186b8e4fc09f20cdf60d560cb8093
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2020-03-14 16:57:07 +0000

    Finished ritual room, landing and bathrooms

---
+++ CitrusX/Assets/Scripts/ReplaceGameObjects.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+/*
+ * http://forum.unity3d.com/threads/24311-Replace-game-object-with-prefab/page2
+ * */
+
+public class ReplaceGameObjects : ScriptableWizard
+
+{
+    public GameObject useGameObject;
+
+    [MenuItem("Custom/Replace GameObjects")]
+
+    static void CreateWizard()
+    {
+        ScriptableWizard.DisplayWizard("Replace GameObjects", typeof(ReplaceGameObjects), "Replace");
+    }
+
+    void OnWizardCreate()
+    {
+        foreach (Transform t in Selection.transforms)
+        {
+            GameObject newObject = PrefabUtility.InstantiatePrefab(useGameObject) as GameObject;
+            Transform newT = newObject.transform;
+
+            newT.position = t.position;
+            newT.rotation = t.rotation;
+            newT.localScale = t.localScale;
+            newT.parent = t.parent;
+
+        }
+
+        foreach (GameObject go in Selection.gameObjects)
+        {
+            DestroyImmediate(go);
+        }
+    }
+}
