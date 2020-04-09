b766cf1b54913677a87010d7dcdf6503800fc93d
Hugo <hugo.filipereis@hotmail.com>
2020-04-09 18:16:18 +0100

    Troubled fixes

--- CitrusX/Assets/Scripts/VolumeControls.cs
+++ CitrusX/Assets/Scripts/VolumeControls.cs
@@ -26,6 +26,26 @@ public class VolumeControls : MonoBehaviour
     private void Awake()
     {
         slider = GetComponent<Slider>();
+        switch (group)
+        {
+            case Groups.MASTER:
+                slider.value = PlayerPrefs.GetFloat("Master");
+                break;
+            case Groups.AMBIENCE:
+                slider.value = PlayerPrefs.GetFloat("Ambience");
+                break;
+            case Groups.BGM:
+                slider.value = PlayerPrefs.GetFloat("BGM");
+                break;
+            case Groups.SFX:
+                slider.value = PlayerPrefs.GetFloat("SFX");
+                break;
+            case Groups.DIALOG:
+                slider.value = PlayerPrefs.GetFloat("Dialog");
+                break;
+            default:
+                break;
+        }
     }
     public Groups group;
 
@@ -37,22 +57,29 @@ public class VolumeControls : MonoBehaviour
         {
             case Groups.MASTER:
                 audioMixer.SetFloat("Master",slider.value);
+                PlayerPrefs.SetFloat("Master", slider.value);
                 break;
             case Groups.AMBIENCE:
                 audioMixer.SetFloat("Ambience", slider.value);
+                PlayerPrefs.SetFloat("Ambience", slider.value);
                 break;
             case Groups.BGM:
                 audioMixer.SetFloat("BGM", slider.value);
+                PlayerPrefs.SetFloat("BGM", slider.value);
                 break;
             case Groups.SFX:
                 audioMixer.SetFloat("SFX", slider.value);
+                PlayerPrefs.SetFloat("SFX", slider.value);
                 break;
             case Groups.DIALOG:
                 audioMixer.SetFloat("Dialog", slider.value);
+                PlayerPrefs.SetFloat("Dialog", slider.value);
                 break;
             default:
                 break;
         }
+
+        PlayerPrefs.Save();
 
 
 
